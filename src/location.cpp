28ec2df0f17a8d7b312c65a7e674e6a057e96d26
alope808 <ashley.lopez.hd@gmail.com>
2025-05-30 22:10:33 -0700

    Changed Location Class

--- src/location.cpp
+++ src/location.cpp
@@ -4,3 +4,31 @@
 #include "item.h"
 #include "clue.h"
 using namespace std;
+
+Location::Location(const string& name, const string& description, vector <string> ItemClues, bool accessible, bool multiple, string keyItem){
+    isAccessible = acsessible;
+    this->name = name;
+    this->description = description;
+    hasMultipleClues = multiple;
+    keyItemFound = false;
+    keyItemId = 0;
+    itemsList = ItemClues;
+}
+
+Location::Location(const string& name, const string& description, bool accessible, bool multiple, string keyItem){
+    isAccessible = acsessible;
+    this->name = name;
+    this->description = description;
+    hasMultipleClues = multiple;
+    keyItemFound = false;
+    keyItemId = 0;
+    itemsList = ItemClues;
+}
+
+bool Location::checkUnlock(){
+    return isAcsessible;
+}
+
+bool Location::checkMultiItems(){
+    return hasMultipleClues;
+}
