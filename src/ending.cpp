e083a8f5aa268381cd214cdc9ec9e24d86d7fda3
naomimenard <nmena004>
2025-06-01 12:39:15 -0700

    Added ending files

--- src/ending.cpp
+++ src/ending.cpp
@@ -1,1 +1,10 @@
 #include "ending.h"
+
+Ending::Ending() {
+    sceneName = "";
+    storyDescription = "";
+    hpcap = 0;
+}
+
+Ending::Ending(string name, string storyDescript, int hp) : sceneName(name), storyDescription(storyDescript), hpcap(hp){}
+
